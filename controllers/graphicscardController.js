40d04ec366f64de67f3ceb17079932445cfd5922
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-11-27 23:53:47 +0530

    added controller for gpu post route

--- controllers/graphicscardController.js
+++ controllers/graphicscardController.js
@@ -1,6 +1,6 @@
 const GraphicsCard = require("../models/graphicscard");
 const asyncHandler = require("express-async-handler");
-
+const { body, validationResult } = require("express-validator");
 // Display list of all BookInstances.
 exports.graphicscard_list = asyncHandler(async (req, res, next) => {
   const allGraphicsCards = await GraphicsCard.find(
@@ -37,14 +37,49 @@ exports.graphicscard_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display gpu create form on GET.
-exports.graphicscard_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Graphics Card create GET");
-});
+exports.graphicscard_create_get = (req, res, next) => {
+  res.render("graphicscard_form", { title: "Create GPU" });
+};
 
 // Handle gpu create on POST.
-exports.graphicscard_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Graphics Card create POST");
-});
+exports.graphicscard_create_post = [
+  // Validate and sanitize fields
+  body("name", "Graphics Card name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  // Add more validations for other fields if necessary
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a graphics card object with escaped and trimmed data.
+    const newGraphicsCard = new GraphicsCard({ name: req.body.name });
+    // Add other fields based on your schema
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("graphicscard_form", {
+        title: "Create Graphics Card",
+        graphicscard: newGraphicsCard,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      try {
+        // Save the new graphics card
+        await newGraphicsCard.save();
+        // Redirect to the newly created graphics card's detail page
+        res.redirect(newGraphicsCard.url);
+      } catch (err) {
+        // Handle any error that occurs during saving the graphics card
+        return next(err);
+      }
+    }
+  }),
+];
 
 // Display gpu delete form on GET.
 exports.graphicscard_delete_get = asyncHandler(async (req, res, next) => {
